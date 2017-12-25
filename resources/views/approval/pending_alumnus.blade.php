004373cff002921079abed67d0622847f7d042b1
Majed Dev <onedevmajed@gmail.com>
2017-12-25 11:29:51 +0300

    Fix pending alumnus page

--- resources/views/approval/pending_alumnus.blade.php
+++ resources/views/approval/pending_alumnus.blade.php
@@ -14,7 +14,7 @@
 
     <!-- Bootstrap core CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <!-- -->
     <link rel="stylesheet" href="/css/home.css">
     <link rel="stylesheet" href="/css/css_login.css">
